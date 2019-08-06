d280f9911167abe3099f105ee86ed5619c965e27
Ryan Baermann <rcbaermann@gmail.com>
2019-08-06 11:22:24 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        class Human {
+
+            public string Name {get;set;}
+            public int Strength {get;set;} = 3;
+            public int Intelligence {get;set;} = 3;
+            public int Dexterity {get;set;} = 3;
+            private int Health {get;set;} = 100;
+            public int health {
+                get { return health; }
+            }
+
+            public Human(string name){
+                Name = name;
+            }
+
+            public Human(string name, int strength, int intelligence, int dexterity, int health){
+                Name = name;
+                Strength = strength;
+                Intelligence = intelligence;
+                Dexterity = dexterity;
+                this.Health = health;
+            }
+
+            public int Attack(Human target){
+                var Enemy = (Human) enemy;
+                Enemy.Health -= 5 * Human.Strength;
+                return Enemy.Health;
+            }
+        }
+    }
+}
